4fdd03ac5a57eeb246d8049277365fc1419c2f4b
Surjeet Mohanty <surjeetmohanty84@gmail.com>
2022-02-19 18:20:46 +0530

    First Commit on Master branch

---
+++ src/MasterHelloOne.java
@@ -0,0 +1,11 @@
+
+public class MasterHelloOne {
+
+	public static String getMessage() {
+		return "Hello Git Master";
+	}
+	public static void main(String[] args) {
+		System.out.println(getMessage());
+	}
+
+}

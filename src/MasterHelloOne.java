2b55c15434d34223a35391345c7b9f297a81de5a
Surjeet Mohanty <surjeetmohanty84@gmail.com>
2022-02-22 17:24:07 +0530

    changes updated

--- src/MasterHelloOne.java
+++ src/MasterHelloOne.java
@@ -2,6 +2,7 @@
 public class MasterHelloOne {
 
 	public static String getMessage() {
+		System.out.println("Hello message");
 		return "Hello Git Master";
 	}
 	public static void main(String[] args) {
